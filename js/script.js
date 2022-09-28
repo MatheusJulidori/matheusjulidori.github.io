f03dabb12583fea1f7036ed6735234fc21f26f44
Matheus Julidori <m.henrique@ges.inatel.br>
2022-09-27 21:16:36 -0300

    Porfolio Update

---
+++ js/script.js
@@ -0,0 +1,4 @@
+document.getElementById("download_cv").addEventListener("click", function downloadCV(){
+    window.open("https://matheusjulidori.github.io/CV_MatheusJulidori.pdf", "_blank");
+}); 
+
