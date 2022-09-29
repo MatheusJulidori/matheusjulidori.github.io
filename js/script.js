533a7c26cda58e174ed64771fbdc0cbf311a4e1d
Matheus Julidori <m.henrique@ges.inatel.br>
2022-09-29 16:30:32 -0300

    V1.1 - Adding slides to porfolio

--- js/script.js
+++ js/script.js
@@ -1,4 +1,31 @@
-document.getElementById("download_cv").addEventListener("click", function downloadCV(){
+document.getElementById("download_cv").addEventListener("click", function downloadCV() {
     window.open("https://matheusjulidori.github.io/CV_MatheusJulidori.pdf", "_blank");
-}); 
+});
 
+// Swiper
+const swiper = new Swiper('.swiper', {
+    loop: true,
+
+    slidesPerView: 1,
+    spaceBetween: 10,
+    breakpoints: {
+        // when window width is >= 480px
+        480: {
+            slidesPerView: 2,
+            spaceBetween: 20
+        },
+        // when window width is >= 640px
+        1240: {
+            slidesPerView: 3,
+            spaceBetween: 20
+        },
+        1600: {
+            slidesPerView: 4,
+            spaceBetween: 20
+        }
+    },
+    navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+    },
+});
